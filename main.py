eedef4a905a96e56e9603e9ba6f425ee7cf6d71b
Karim <77207877+poggersbutnot@users.noreply.github.com>
2021-07-06 22:14:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from webbrowser import open
 from sys import exit
 
 QIU = False #quit_if_update
+codes = ['Seecashinc','CAPITALISTIC', 'Cashinc50k']
 
 print(get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/updates').text.replace('\n', ''))
-ver = """1.0
+ver = """1.1
 """
 if ver != get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/version').text:
     print("New update available...")
@@ -25,11 +26,21 @@ else:
 
 def repeat():
     for i in range(1200000):
-        a = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': 'Cashinc50k'})
+        a = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[0]})
+        b = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[1]})
+        c = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[2]})
         if 'You can only redeem the same code once' in a.text:
-            print("User "+ str(i) + " has already claimed this code.")
+            print("User "+ str(i) + " has already claimed this code: ", str(codes[0]))
         else:
-            print("Given to ID:", str(i))
+            print("Given to "+ codes[0] + " to ID:", str(i))
+        if 'You can only redeem the same code once' in b.text:
+            print("User "+ str(i) + " has already claimed this code: ", str(codes[1]))
+        else:
+            print("Given to Code "+ codes[1] + " to ID:", str(i))
+        if 'You can only redeem the same code once' in c.text:
+            print("User "+ str(i) + " has already claimed this code: ", str(codes[2]))
+        else:
+            print("Given "+ codes[2] + " to ID:", str(i))
 
 if __name__ == "__main__":
     for i in range(1):
