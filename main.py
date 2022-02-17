047b7d8986fa065d6c8ad8c847de4396dd2dbfe4
Karim <77207877+poggersbutnot@users.noreply.github.com>
2022-02-17 20:05:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
-from requests import post, get
-from threading import Thread
-from webbrowser import open
-from sys import exit
+from requests import post as _post
+from sys import argv as _argv
 
-QIU = False #quit_if_update
-codes = ['Seecashinc','CAPITALISTIC', 'Cashinc50k']
+__all__ = ('codes', 'repeat')
 
-print(get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/updates').text.replace('\n', ''))
-ver = """1.1
-"""
-if ver != get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/version').text:
-    print("New update available...")
-    print("Update "+ get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/version').text.replace('\n', '') + " is availible.")
-    open("https://github.com/poggersbutnot/CashIncGiveCodes")
-    if QIU is True:
-        exit()
-    else:
-        pass
-    if ver.replace('.', '') > get('https://raw.githubusercontent.com/poggersbutnot/CashIncGiveCodes/main/version').text.replace('.', ''):
-        print("So u decided to change the version, but WHY.")
-    else:
-        pass
-else:
-    pass
+logging = False
+if len(_argv) > 1:
+    from time import time as _time
+    logging = True
+    _cash_file = open('Log_%i_CashInc.log' % round(_time()), 'a')
+    del _time
+
+del _argv
+
+codes = ['Seecashinc', 'CAPITALISTIC', 'Cashinc50k']
+
 
 def repeat():
-    for i in range(1200000):
-        a = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[0]})
-        b = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[1]})
-        c = post('https://www.alegrium.com/cash/index.php/promo/code/'+ str(i), data={'PromoCodeForm[promo_code]': codes[2]})
-        if 'You can only redeem the same code once' in a.text:
-            print("User "+ str(i) + " has already claimed this code: ", str(codes[0]))
-        else:
-            print("Given Code "+ codes[0] + " to ID:", str(i))
-        if 'You can only redeem the same code once' in b.text:
-            print("User "+ str(i) + " has already claimed this code: ", str(codes[1]))
-        else:
-            print("Given Code "+ codes[1] + " to ID:", str(i))
-        if 'You can only redeem the same code once' in c.text:
-            print("User "+ str(i) + " has already claimed this code: ", str(codes[2]))
-        else:
-            print("Given Code "+ codes[2] + " to ID:", str(i))
+    i = 0
+    while True:
+        i += 1
+        for n in range(len(codes)):
+            a = _post(f"https://www.alegrium.com/cash/index.php/promo/code/{i}",
+                      data={'PromoCodeForm[promo_code]': codes[n]})
+            if 'You can only redeem the same code once' in a.text:
+                if logging:
+                    _cash_file.write(
+                        f"User {i} has already claimed the code {codes[n]}\n"
+                    )
+                    _cash_file.flush()
+                print("User %i has already claimed the code %s" %
+                      (i, codes[n]))
+            else:
+                if logging:
+                    _cash_file.write(
+                        "Given Code {} to user {}\n".format(codes[n], i)
+                    )
+                    _cash_file.flush()
+                print("Given Code %s to user %i" % (codes[n], i))
+
 
 if __name__ == "__main__":
-    for i in range(1):
-       Thread(target=repeat).start()
+    repeat()
